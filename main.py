01acb69b0283f94b196b5ba968a13812f1066ddf
JoaoVictor <jmachadoferraz@gmail.com>
2025-03-10 16:37:43 -0300

    Movimentar blocos

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pygame, sys
-from grid import Grid
-from blocks import *
+import pygame,sys
+from game import Game
 
 pygame.init()
 purple = (87, 9, 117)
@@ -10,20 +9,26 @@ pygame.display.set_caption("Tetris com Python")
 
 clock = pygame.time.Clock()
 
-game_grid = Grid()
-
-block = IBlock()
+game = Game()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+               game.move_left()
+            if event.key == pygame.K_RIGHT:
+               game.move_right()
+            if event.key == pygame.K_DOWN:
+               game.move_down()
+            
+
     
     #Drawing the display
     screen.fill(purple)
-    game_grid.draw(screen)
-    block.draw(screen)
+    game.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
